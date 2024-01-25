228a3ee4d817f695d38a2a6c3d409ead1582c4b8
Danil <dandy0957@gmail.com>
2024-01-25 21:07:39 +0200

    2

--- UdemyProject/UdemyProject/views.py
+++ UdemyProject/UdemyProject/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def first_page(request):
-    a = "Новая страница1"
+    a = "Новая страница2"
     text_index = "Something index text"
     return render(request, './index.html',{
         'a': a,
