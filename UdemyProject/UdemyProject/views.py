8069914754355f68e3c66f4072ddb851af62e696
Danil <dandy0957@gmail.com>
2024-01-29 18:54:52 +0200

    Добавил страницу спсибо и переход на страницу спасибо при отправке формы

--- UdemyProject/UdemyProject/views.py
+++ UdemyProject/UdemyProject/views.py
@@ -2,10 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def first_page(request):
-    a = "Новая страница2"
-    text_index = "Something index text"
-    return render(request, './index.html',{
-        'a': a,
-        'text': text_index
-    })
+
--- UdemyProject/crm/views.py
+++ UdemyProject/crm/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Order
 # Create your views here.
+def first_page(request):
+    object_list = Order.objects.all()
+    return render(request, './index.html', {'object_list' : object_list})
+
+def thanks_page(reguest):
+    name = reguest.POST['name']
+    phone = reguest.POST['phone']
+    element = Order(order_name= name, order_phone = phone)
+    element.save()
+    return render(reguest, './thanks_page.html', {'name':name,
+                                                                      'phone': phone})
